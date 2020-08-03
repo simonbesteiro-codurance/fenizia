3ac5c936f0941dd62416b44c59a9e9dbf10d4e7c
Dani Alcala <daniamcode@gmail.com>
2020-08-03 20:24:33 +0200

    small slider from dashboard done without logic

--- fenizia-react/src/components/Slider.js
+++ fenizia-react/src/components/Slider.js
@@ -1,8 +1,137 @@
 import React from "react";
+import "./Slider.css";
 
-function Slider (){
-    return <>
-     <p>Slider</p>
-    </>
+function Slider() {
+  return (
+    <section className="slider">
+      <div className="slider-text">
+        <p className="slider-books">LIBROS - Novedades</p>
+        <p className="slider-view-all">VER TODO</p>
+      </div>
+      <div className="slider-content">
+        <div className="list__item">
+          <img
+            alt="Alerta Roja"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
+            title="Alerta Roja"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Buffaloed"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
+            title="Buffaloed"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="The Gentlemen: Los señores de la mafia"
+            className="artwork__image"
+            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
+            title="The Gentlemen: Los señores de la mafia"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Alerta Roja"
+            className="artwork__image"
+            src="https://imagessl9.casadellibro.com/a/l/t1/49/9788466667449.jpg"
+            title="Alerta Roja"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Buffaloed"
+            className="artwork__image"
+            src="https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg"
+            title="Buffaloed"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="The Gentlemen: Los señores de la mafia"
+            className="artwork__image"
+            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
+            title="The Gentlemen: Los señores de la mafia"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Alerta Roja"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
+            title="Alerta Roja"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Buffaloed"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
+            title="Buffaloed"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="The Gentlemen: Los señores de la mafia"
+            className="artwork__image"
+            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
+            title="The Gentlemen: Los señores de la mafia"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Alerta Roja"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
+            title="Alerta Roja"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Buffaloed"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
+            title="Buffaloed"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="The Gentlemen: Los señores de la mafia"
+            className="artwork__image"
+            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
+            title="The Gentlemen: Los señores de la mafia"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Alerta Roja"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
+            title="Alerta Roja"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="Buffaloed"
+            className="artwork__image"
+            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
+            title="Buffaloed"
+          />
+        </div>
+        <div className="list__item">
+          <img
+            alt="The Gentlemen: Los señores de la mafia"
+            className="artwork__image"
+            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
+            title="The Gentlemen: Los señores de la mafia"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
+
 export default Slider;
--- fenizia-react/src/product.mock.js
+++ fenizia-react/src/product.mock.js
@@ -1,9 +1,12 @@
 const productList = [
-	{ id: 1, product: {type: "book", title: 'El Principito', author: 'Antoine de Saint-Exúpery', price: 12} },
-	{ id: 2, product: {type: "book", title: 'El Quijote', author: 'Miguel de Cervantes', price: 15} },
-	{ id: 3, product: {type: "book", title: 'Crímen y Castigo', author: 'Fiodor Dostoyevski', price: 17} },
-	{ id: 4, product: {type: "book", title: 'El señor de los anillos', author: 'J.R.R Tolkien', price: 12} },
-	{ id: 5, product: {type: "book", title: 'Clean Code', author: 'Gilberto Cao', price: 37} },
+    { id: 1, product: {type: "book", title: 'Postales del Este', author: 'Reyes Monforte', price: 19.85, rating: 9, cover: "./img/postales-del-este.jpg"} },
+	{ id: 2, product: {type: "book", title: 'Un cuento perfecto', author: 'Elisabet Benavent', price: 17, rating: 8.5, cover: "./img/un-cuento-perfecto.jpg"} },
+	{ id: 3, product: {type: "book", title: 'El infinito en un junco', author: 'Irene Vallejo', price: 23.7, rating: 7.5, cover: "./img/el-infinito-en-un-junco.jpg"} },
+	{ id: 4, product: {type: "book", title: 'El mensaje de Pandora', author: 'Javier Serra', price: 15.1, rating: 5, cover: "./img/el-mensaje-de-pandora.jpg"} },
+	{ id: 5, product: {type: "book", title: 'El mentiroso', author: 'Mikel Santiago', price: 19.85, rating: 8, cover: "./img/el-mentiroso.jpg"} },
+	{ id: 6, product: {type: "book", title: 'Loba negra', author: 'Juan Gómez-Jurado', price: 19.85, rating: 7.5, cover: "./img/loba-negra.jpg"} },
+	{ id: 7, product: {type: "book", title: 'Reina roja', author: 'Juan Gómez-Jurado', price: 15, rating: 6.5, cover: "./img/reina-roja.jpg"} },
+	{ id: 8, product: {type: "book", title: 'Una noche muy larga', author: 'Dov Alfon', price: 19.95, rating: 7.5, cover: "./img/una-noche-muy-larga.jpg"} },
 ];
 
 export default productList
