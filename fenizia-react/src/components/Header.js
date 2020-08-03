d13bb337eb6869120bc940ad7fe4f31d17242ce3
Esther Morillo <esther.morillo@gmail.com>
2020-08-03 20:24:40 +0200

    Create icons and logo

--- fenizia-react/src/components/Header.js
+++ fenizia-react/src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css'
+
 
 
 function Header() {
-    return <p>Navegador Fenizia</p>
+    return (
+        <section className="main-nav">
+            <NavLink to="/" className="icon__hamburger"     activeClassName="nav__link--active">
+            </NavLink>
+
+            <NavLink className="icon__logo" activeClassName="nav__link--active">
+            </NavLink> 
+
+            <NavLink to="/" className="icon__login" activeClassName="nav__link--active">
+            </NavLink>
+
+            <NavLink to="/" className="icon__bag" activeClassName="nav__link--active">
+            </NavLink>
+        </section>    
+    )
 }
 
 export default Header;
