5e1c03dd16a48ddd0aec55a79abba92ee38b3f3f
Dani Alcala <daniamcode@gmail.com>
2020-08-03 23:47:31 +0200

    slider from dashboard done with flux

--- fenizia-react/src/components/Slider.js
+++ fenizia-react/src/components/Slider.js
@@ -1,134 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import productStore from "../stores/productStore";
+import { loadProducts } from "../actions/productActions";
 import "./Slider.css";
 
 function Slider() {
+  const [products, setProducts] = useState(productStore.getProduct());
+
+  useEffect(() => {
+    productStore.addChangeListener(onChange);
+    if (products.length === 0) loadProducts();
+    return () => productStore.removeChangeListener(onChange);
+  }, [products.length]);
+
+  function onChange() {
+    setProducts(productStore.getProduct());
+  }
+
+  // slice must be done in the store!
+  const mainProducts = products.slice(0, 10);
+
   return (
     <section className="slider">
-      <div className="slider-text">
-        <p className="slider-books">LIBROS - Novedades</p>
-        <p className="slider-view-all">VER TODO</p>
+      <div className="slider__text">
+        <p className="slider__title">LIBROS - Novedades</p>
+        <p className="slider__view-all">VER TODO</p>
       </div>
-      <div className="slider-content">
-        <div className="list__item">
-          <img
-            alt="Alerta Roja"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
-            title="Alerta Roja"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Buffaloed"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
-            title="Buffaloed"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="The Gentlemen: Los señores de la mafia"
-            className="artwork__image"
-            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
-            title="The Gentlemen: Los señores de la mafia"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Alerta Roja"
-            className="artwork__image"
-            src="https://imagessl9.casadellibro.com/a/l/t1/49/9788466667449.jpg"
-            title="Alerta Roja"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Buffaloed"
-            className="artwork__image"
-            src="https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg"
-            title="Buffaloed"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="The Gentlemen: Los señores de la mafia"
-            className="artwork__image"
-            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
-            title="The Gentlemen: Los señores de la mafia"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Alerta Roja"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
-            title="Alerta Roja"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Buffaloed"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
-            title="Buffaloed"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="The Gentlemen: Los señores de la mafia"
-            className="artwork__image"
-            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
-            title="The Gentlemen: Los señores de la mafia"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Alerta Roja"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
-            title="Alerta Roja"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Buffaloed"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
-            title="Buffaloed"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="The Gentlemen: Los señores de la mafia"
-            className="artwork__image"
-            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
-            title="The Gentlemen: Los señores de la mafia"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Alerta Roja"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg"
-            title="Alerta Roja"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="Buffaloed"
-            className="artwork__image"
-            src="https://imagessl0.casadellibro.com/a/l/t1/90/9788417860790.jpg"
-            title="Buffaloed"
-          />
-        </div>
-        <div className="list__item">
-          <img
-            alt="The Gentlemen: Los señores de la mafia"
-            className="artwork__image"
-            src="https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg"
-            title="The Gentlemen: Los señores de la mafia"
-          />
-        </div>
+      <div className="slider__content">
+        {mainProducts.map((element) => (
+          <div key={element.id} className="slider__item">
+            <img
+              key={element.id}
+              alt={element.product.title}
+              className="slider__image"
+              src={element.product.cover}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
