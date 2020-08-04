9577f4f62c65d9acf3aba47e070c058b3c39ff12
Esther Morillo <esther.morillo@gmail.com>
2020-08-04 23:31:32 +0200

    Image cover

--- fenizia-react/src/components/Cover.js
+++ fenizia-react/src/components/Cover.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import ReactPlayer from 'react-player';
 import './Cover.css'
 
 function Cover () {
     return (
         
-        <div className='video-cover'>
-            <ReactPlayer
-                
-                url={require('../video/video-cover.mp4')}
-                playing='true'
-                width='100%'
-                height='100%'
-            />
+        <div className='img__cover'>
+            <img src={require('../img/img-cover.jpg')} alt=""/>
+            <p className='img__title'>Leer nos hace libres</p>
         </div>
     )
 }   
