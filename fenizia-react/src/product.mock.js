618b7403995a363d81b3c6a5bb4c537dfb4734fd
Esther Morillo <esther.morillo@gmail.com>
2020-08-04 18:54:45 +0200

    create list books

--- fenizia-react/src/product.mock.js
+++ fenizia-react/src/product.mock.js
@@ -8,10 +8,8 @@ const productList = [
 				price: 19.85, 
 				rating: 9, 
 				cover: "https://imagessl0.casadellibro.com/a/l/t1/90/9788401023590.jpg", 	description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
-				properties: {
-					newRelease: false,
-					bestseller: true
-				}
+				newRelease: false,
+				bestseller: true				
 			} 
 	},
 	{ 
@@ -23,7 +21,9 @@ const productList = [
 			price: 17, 
 			rating: 8.5, 
 			cover: "https://imagessl8.casadellibro.com/a/l/t1/98/9788418038198.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: true,
+			bestseller: false
 		} 
 	},
 	{ 
@@ -32,10 +32,12 @@ const productList = [
 			type: "book", 
 			title: 'El infinito en un junco', 
 			author: 'Irene Vallejo', 
-			price: 23.7, 
+			price: 23.70, 
 			rating: 7.5, 
 			cover: "https://imagessl8.casadellibro.com/a/l/t1/38/9788466346238.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -44,10 +46,12 @@ const productList = [
 			type: "book", 
 			title: 'El mensaje de Pandora', 
 			author: 'Javier Serra', 
-			price: 15.1, 
+			price: 15.10, 
 			rating: 5, 
 			cover: "https://imagessl9.casadellibro.com/a/l/t1/49/9788466667449.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -59,7 +63,9 @@ const productList = [
 			price: 19.85, 
 			rating: 8, 
 			cover: "https://imagessl5.casadellibro.com/a/l/t1/65/9788484452065.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -71,7 +77,9 @@ const productList = [
 			price: 19.85, 
 			rating: 7.5, 
 			cover: "https://imagessl7.casadellibro.com/a/l/t1/97/9788466666497.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: true,
+			bestseller: false
 		}
 	},
 	{ 
@@ -81,9 +89,11 @@ const productList = [
 			title: 'Reina roja', 
 			author: 'Juan Gómez-Jurado', 
 			price: 15, 
-			rating: 6.5, 
+			rating: 6.50, 
 			cover: "https://imagessl3.casadellibro.com/a/l/t1/23/9788499926223.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -95,7 +105,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -107,7 +119,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl5.casadellibro.com/a/l/t1/95/9788467058895.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -119,7 +133,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl8.casadellibro.com/a/l/t1/98/9788498381498.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: true,
+			bestseller: false
 		}
 	},
 	{ 
@@ -131,7 +147,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl6.casadellibro.com/a/l/t1/26/9788425432026.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -143,7 +161,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: true,
+			bestseller: false
 		}
 	},
 	{ 
@@ -155,7 +175,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -167,7 +189,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: false,
+			bestseller: true
 		}
 	},
 	{ 
@@ -179,7 +203,9 @@ const productList = [
 			price: 19.95, 
 			rating: 7.5, 
 			cover: "https://imagessl2.casadellibro.com/a/l/t1/32/9788408232032.jpg",
-			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.'
+			description: 'Una emocionante historia basada en hechos reales sobre la memoria, el amor y la esperanza en medio del horror de Auschwitz.',
+			newRelease: true,
+			bestseller: false
 		}
 	}
 ];
