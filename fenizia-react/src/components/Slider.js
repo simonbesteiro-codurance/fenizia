727ab0d10b2d72ecccd30ebd411767054dc638af
Dani Alcala <daniamcode@gmail.com>
2020-08-04 12:03:27 +0200

    slider updated with text for each image

--- fenizia-react/src/components/Slider.js
+++ fenizia-react/src/components/Slider.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import productStore from "../stores/productStore";
 import { loadProducts } from "../actions/productActions";
 import "./Slider.css";
@@ -18,22 +19,34 @@ function Slider() {
 
   // slice must be done in the store!
   const mainProducts = products.slice(0, 10);
+  const currency = 'euros'
 
   return (
     <section className="slider">
       <div className="slider__text">
+        <Link to={'/novedades'}>
         <p className="slider__title">LIBROS - Novedades</p>
+        </Link>
+        <Link to={'/novedades'}>
         <p className="slider__view-all">VER TODO</p>
+        </Link>
       </div>
       <div className="slider__content">
         {mainProducts.map((element) => (
-          <div key={element.id} className="slider__item">
-            <img
-              key={element.id}
-              alt={element.product.title}
-              className="slider__image"
-              src={element.product.cover}
-            />
+          <div className="slider__content-wrapper">
+            <div key={element.id} className="slider__item">
+              <img
+                key={element.id}
+                alt={element.product.title}
+                className="slider__image"
+                src={element.product.cover}
+              />
+              <div key={element.id} className="slider__item-text">
+                <div key={element.id}>{element.product.title}</div>
+                <div key={element.id}>{element.product.author}</div>
+                <div key={element.id}>{element.product.price} {currency} || Valoración: {element.product.rating}</div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
