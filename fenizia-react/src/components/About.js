6fd475e5eae0661fcb9f17fbffe8de5e2393dcdf
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-08-07 14:36:01 +0200

    update about

--- fenizia-react/src/components/About.js
+++ fenizia-react/src/components/About.js
@@ -1,30 +1,38 @@
 import React from "react";
-import './About.css';
-
+import "./About.css";
 
 function AboutUs() {
   return (
     <>
-      <h2 className='title'>Welcome to Fenizia</h2>
+      <h2 className="title">Welcome to Fenizia</h2>
       <div className="img__contain">
         <div className="img__block">
-          <img className="img__small" src="https://ca.slack-edge.com/T0SJKHBFZ-U013V09GQ9H-6f4454feee02-512"></img>
+          <img
+            className="img__small"
+            src="https://ca.slack-edge.com/T0SJKHBFZ-U013V09GQ9H-6f4454feee02-512"
+          ></img>
           <p>Dani Alcalà</p>
         </div>
         <div className="img__block">
-          <img className="img__small" src="https://ca.slack-edge.com/T0SJKHBFZ-USCNAUTBL-e9acd67da6ce-512"></img>
+          <img
+            className="img__small"
+            src="https://ca.slack-edge.com/T0SJKHBFZ-USCNAUTBL-e9acd67da6ce-512"
+          ></img>
           <p>Simón Fernández</p>
         </div>
         <div className="img__block">
-          <img className="img__small" src="https://ca.slack-edge.com/T0SJKHBFZ-U0106J9A473-c55b8880f483-512"></img>
+          <img
+            className="img__small"
+            src="https://ca.slack-edge.com/T0SJKHBFZ-U0106J9A473-c55b8880f483-512"
+          ></img>
           <p>Esther Morillo</p>
         </div>
       </div>
       <p className="footer-text">
-        This is a project made on Skylab Coders academy during the 202007
-        Bootcamp.
+        This is a project made <b>in 3 days</b> on{" "}
+        <a href="https://www.skylabcoders.com/es">Skylab Coders academy</a>{" "}
+        during the 202007 Bootcamp.
       </p>
-      
     </>
   );
 }
